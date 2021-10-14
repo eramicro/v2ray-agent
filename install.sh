ce47506fb94010d2abf50e9007c0c81eb54487a9
eramicro <44019323+eramicro@users.noreply.github.com>
2021-10-14 17:33:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4267,10 +4267,7 @@ switchAlpn() {
 menu() {
 	cd "$HOME" || exit
 	echoContent red "\n=============================================================="
-	echoContent green "作者：mack-a"
-	echoContent green "当前版本：v2.5.36"
-	echoContent green "Github：https://github.com/mack-a/v2ray-agent"
-	echoContent green "描述：八合一共存脚本\c"
+	echoContent green "当前版本：v2.5.36 2021/10/14
 	showInstallStatus
 	echoContent red "\n=============================================================="
 	if [[ -n "${coreInstallType}" ]]; then
